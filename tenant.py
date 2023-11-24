5147ea24e916f4db68d655d2af54b6ddc52827d5
Nathan Seixeiro <108758693+nathanSeixeiro@users.noreply.github.com>
2023-11-24 13:17:05 +0000

    add some important informations that was missing

--- tenant.py
+++ tenant.py
@@ -12,6 +12,9 @@ def read_excel_archive(archive_name, info_tenants):
         tenant = str(row['LOCATÁRIO (INQUILINO)'])  # Convertendo para string
         amount = round(row['VALOR BOLETO'], 2) or 0   # arredondar 2 casas decimais
         transfer_amount = round(row['REPASSE FINAL'], 2) or 0  
+        rent = round(row['ALUGUEL'], 2) or 0  
+        iptu = round(row['IPTU'], 2) or 0  
+        cond = row['CONDOMÍNIO']
         assessment_discont = round(row['MULTA / DESCONTO'],2) or 0
         consume_bills = row['CONTAS DE CONSUMO']
         adm_amount = round(row['VALOR ADM'],2)
@@ -28,12 +31,15 @@ def read_excel_archive(archive_name, info_tenants):
         if isinstance(tenant, str) :
            info_tenants.append({
                 'Locatário': tenant,
+                'Aluguel': rent,
                 'Valor Boleto': amount,
                 'Repasse': transfer_amount,
                 'Consumo': consume_bills,
+                'Condominio': cond,
                 'Água': water_bill,
                 'Luz': light_bill,
                 'Gás': gas_bill,
+                'IPTU': iptu,
                 'Valor ADM': adm_amount,
                 'Multa/Desconto': assessment_discont,
                 'Situação': situation,
@@ -42,14 +48,14 @@ def read_excel_archive(archive_name, info_tenants):
 
 def showAllRecords(info_locatarios_sorted):
     # Exibir informações ordenadas
-    for info in info_locatarios_sorted:
-        print(f"\nLocatário: {info['Locatário']},\n  Valor do boleto: {info['Valor Boleto']},\n  Valor Repasse: {info['Repasse']},\n  Água: {info['Água']},\n  Luz: {info['Luz']},\n  Gás:{info['Gás']},\n  Valor ADM: {info['Valor ADM']},\n  Multa/Desconto: {info['Multa/Desconto']},\n  Situação no mês de {info['Mês']}: {info['Situação']}\n ")
+    for info in info_locatarios_sorted:  
+        print(f"\nLocatário: {info['Locatário']},\n  Aluguel: {info['Aluguel']},\n  Valor do boleto: {info['Valor Boleto']},\n  Valor Repasse: {info['Repasse']},\n  Água: {info['Água']},\n  Luz: {info['Luz']},\n  Gás:{info['Gás']},\n  Condominio: {info['Condominio']},\n  IPTU: {info['IPTU']},\n Valor ADM: {info['Valor ADM']},\n  Multa/Desconto: {info['Multa/Desconto']},\n  Situação no mês de {info['Mês']}: {info['Situação']}\n ")
 
 def showSomeRecords(info_locatarios_sorted):
     # exibi 10 registros
-    for i in range(min(50, len(info_locatarios_sorted))):
+    for i in range(min(150, len(info_locatarios_sorted))):
         info = info_locatarios_sorted[i]
-        print(f"\nLocatário: {info['Locatário']},\n  Valor do boleto: {info['Valor Boleto']},\n  Valor Repasse: {info['Repasse']},\n  Água: {info['Água']},\n  Luz: {info['Luz']},\n  Gás:{info['Gás']},\n Valor ADM: {info['Valor ADM']},\n  Multa/Desconto: {info['Multa/Desconto']},\n  Situação no mês de {info['Mês']}: {info['Situação']}\n ")
+        print(f"\nLocatário: {info['Locatário']},\n  Aluguel: {info['Aluguel']},\n  Valor do boleto: {info['Valor Boleto']},\n  Valor Repasse: {info['Repasse']},\n  Água: {info['Água']},\n  Luz: {info['Luz']},\n  Gás:{info['Gás']},\n  Condominio: {info['Condominio']},\n  IPTU: {info['IPTU']},\n Valor ADM: {info['Valor ADM']},\n  Multa/Desconto: {info['Multa/Desconto']},\n  Situação no mês de {info['Mês']}: {info['Situação']}\n ")
 
 # Caminho relativo para o arquivo na mesma pasta
 archive = os.path.abspath('./JOACIR ROCHA SETEMBRO 23 - copia.xlsx')  # Obtém o caminho absoluto
