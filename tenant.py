07f92119c28005dd7958f9967e112092f612a6be
Nathan Seixeiro <108758693+nathanSeixeiro@users.noreply.github.com>
2023-11-27 18:12:30 +0000

    change archive

--- tenant.py
+++ tenant.py
@@ -10,24 +10,32 @@ def read_excel_archive(archive_name, info_tenants):
     # Iterar sobre as linhas do DataFrame
     for index, row in df.iterrows():
         tenant = str(row['LOCATÁRIO (INQUILINO)'])  # Convertendo para string
-        amount = round(row['VALOR BOLETO'], 2) or 0   # arredondar 2 casas decimais
-        transfer_amount = round(row['REPASSE FINAL'], 2) or 0  
-        rent = round(row['ALUGUEL'], 2) or 0  
-        iptu = round(row['IPTU'], 2) or 0  
+        # amount = round(row['VALOR BOLETO'], 2) or 0   # arredondar 2 casas decimais
+        amount = row['VALOR BOLETO'] or 0   # arredondar 2 casas decimais
+        transfer_amount = row['REPASSE FINAL'] or 0  
+        rent = row['ALUGUEL'] or 0  
+        iptu = row['IPTU'] or 0  
         cond = row['CONDOMÍNIO']
-        assessment_discont = round(row['MULTA / DESCONTO'],2) or 0
+        assessment_discont = row['MULTA / DESCONTO'] or 0.0
         consume_bills = row['CONTAS DE CONSUMO']
-        adm_amount = round(row['VALOR ADM'],2)
-        situation = row['SITUAÇÃO']
-        water_bill = row['ÁGUA ']
-        light_bill = row['LUZ']
-        gas_bill = row['GÁS ']
+        adm_amount = row['VALOR ADM'] or 0.0 
+        situation = row['SITUAÇÃO'] or 0.0
+        water_bill = row['ÁGUA '] or 0.0
+        light_bill = row['LUZ'] or 0.0
+        gas_bill = row['GÁS '] or 0.0
 
         # Se a situação estiver vazia ou não for 'pago', definir como 'rever'
         situation = 'REVER' if pd.isna(situation) or situation.lower() != 'pago' else situation
 
-        # Se o nome for valido e se tiver contas de consumo  
+        roundIfIsNan(amount)
+        roundIfIsNan(transfer_amount)
+        roundIfIsNan(rent)
+        roundIfIsNan(iptu)
+        roundIfIsNan(cond)
+        roundIfIsNan(assessment_discont)
+        roundIfIsNan(adm_amount)
 
+        # Se o nome for valido e se tiver contas de consumo  
         if isinstance(tenant, str) :
            info_tenants.append({
                 'Locatário': tenant,
@@ -46,6 +54,12 @@ def read_excel_archive(archive_name, info_tenants):
                 'Mês': month
            })
 
+def roundIfIsNan(value):
+    if pd.isna(value):
+        value = 0
+    else:
+        round(value,2)
+
 def showAllRecords(info_locatarios_sorted):
     # Exibir informações ordenadas
     for info in info_locatarios_sorted:  
@@ -53,12 +67,12 @@ def showAllRecords(info_locatarios_sorted):
 
 def showSomeRecords(info_locatarios_sorted):
     # exibi 10 registros
-    for i in range(min(150, len(info_locatarios_sorted))):
+    for i in range(min(20, len(info_locatarios_sorted))):
         info = info_locatarios_sorted[i]
         print(f"\nLocatário: {info['Locatário']},\n  Aluguel: {info['Aluguel']},\n  Valor do boleto: {info['Valor Boleto']},\n  Valor Repasse: {info['Repasse']},\n  Água: {info['Água']},\n  Luz: {info['Luz']},\n  Gás:{info['Gás']},\n  Condominio: {info['Condominio']},\n  IPTU: {info['IPTU']},\n Valor ADM: {info['Valor ADM']},\n  Multa/Desconto: {info['Multa/Desconto']},\n  Situação no mês de {info['Mês']}: {info['Situação']}\n ")
 
 # Caminho relativo para o arquivo na mesma pasta
-archive = os.path.abspath('./JOACIR ROCHA SETEMBRO 23 - copia.xlsx')  # Obtém o caminho absoluto
+archive = os.path.abspath('./JOACIR ROCHA OUTUBRO 23 - copia.xlsx')  # Obtém o caminho absoluto
 
 info_tenant = []
 
@@ -67,3 +81,4 @@ read_excel_archive(archive, info_tenant)
 info_locatarios_sorted = sorted(info_tenant, key=lambda x: x['Locatário'])
 
 showSomeRecords(info_locatarios_sorted)
+# showAllRecords(info_locatarios_sorted)
